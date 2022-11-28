8ecd94b754e5b49997f74f4975a2ad72fd1532b5
ivan_sm@hotmail.es <ivan_sm@hotmail.es>
2022-11-28 23:03:49 +0100

    Initial Commit

--- app/src/main/java/com/example/proyectoandroid1eva_ivansm/PantallaDatos.java
+++ app/src/main/java/com/example/proyectoandroid1eva_ivansm/PantallaDatos.java
@@ -2,13 +2,56 @@ package com.example.proyectoandroid1eva_ivansm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class PantallaDatos extends AppCompatActivity {
+
+    private Spinner spinGenero;
+    private Spinner spinProvincia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_datos);
+
+        spinGenero = (Spinner)findViewById(R.id.spinGenero);
+        spinProvincia = (Spinner)findViewById(R.id.spinProvincias);
+
+        ArrayAdapter<CharSequence> arrayGenero = ArrayAdapter.createFromResource(this, R.array.genero, android.R.layout.simple_spinner_item);
+        spinGenero.setAdapter(arrayGenero);
+        ArrayAdapter<CharSequence> arrayProvincia = ArrayAdapter.createFromResource(this, R.array.provincias, android.R.layout.simple_spinner_item);
+        spinProvincia.setAdapter(arrayProvincia);
+    }
+
+    //Metodo para mostrar y ocultar el ActionBar
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.actionbar, menu);
+        return true;
+    }
+
+    //Metodo para asignar las funciones de los items del Action Bar
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.item01){
+            Toast.makeText(this, "Ayuda", Toast.LENGTH_SHORT).show();
+        }else if (id == R.id.item02){
+            Toast.makeText(this,"Acerca de ...", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Metodo que hace que el boton al que se le aplique redireccione al activity pantalla de test
+    public void irPantallaTest (View view) {
+
+        Intent i = new Intent(this, PantallaTest.class);
+
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/proyectoandroid1eva_ivansm/PantallaTest.java
+++ app/src/main/java/com/example/proyectoandroid1eva_ivansm/PantallaTest.java
@@ -2,32 +2,20 @@ package com.example.proyectoandroid1eva_ivansm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
-import android.widget.VideoView;
 
-
-public class MainActivity extends AppCompatActivity {
-
-    private Button btnEmpezar;
-    private VideoView fondoBienvenido;
+public class PantallaTest extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        btnEmpezar = (Button) findViewById(R.id.btnEmpezar);
-
+        setContentView(R.layout.activity_pantalla_test);
     }
 
+    //////////////// Action Bar ////////////////
     //Metodo para mostrar y ocultar el ActionBar
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.actionbar, menu);
@@ -43,13 +31,5 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this,"Acerca de ...", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
-    }
-    
-    //Metodo que hace que el boton al que se le aplique redireccione al activiti pantalla de datos
-    public void irPantallaDatos (View view) {
-
-        Intent i = new Intent(this, PantallaDatos.class);
-
-        startActivity(i);
     }
 }
