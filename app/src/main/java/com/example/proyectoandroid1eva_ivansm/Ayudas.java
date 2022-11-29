ef128ab7c338dea569418ae8391a57f677a1b93d
ivan_sm@hotmail.es <ivan_sm@hotmail.es>
2022-11-29 17:23:11 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/proyectoandroid1eva_ivansm/Ayudas.java
@@ -0,0 +1,14 @@
+package com.example.proyectoandroid1eva_ivansm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ayudas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ayudas);
+    }
+}
