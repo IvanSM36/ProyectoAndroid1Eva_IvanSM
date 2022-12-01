fb3373f5f926df0b123e8e751e0bbbd401811e36
ivan_sm@hotmail.es <ivan_sm@hotmail.es>
2022-12-02 00:39:42 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/proyectoandroid1eva_ivansm/PantallaResultado.java
@@ -0,0 +1,14 @@
+package com.example.proyectoandroid1eva_ivansm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaResultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_resultado);
+    }
+}
